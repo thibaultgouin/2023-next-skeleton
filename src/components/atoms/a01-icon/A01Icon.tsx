6ab64c266dc21fd9706096d5c4469262d99ce49a
Thibault Gouin <thibaultgouin@gmail.com>
2023-02-08 17:27:20 +0100

    Add className props to A01Icon

--- src/components/atoms/a01-icon/A01Icon.tsx
+++ src/components/atoms/a01-icon/A01Icon.tsx
@@ -1,12 +1,14 @@
-import styles from "./A01Icon.module.scss";
+import classNames from "clsx";
 import { IconType } from "react-icons/lib/cjs";
+import styles from "./A01Icon.module.scss";
 
 export interface A01IconProps {
+  className?: string;
   icon: IconType;
 }
 
-const A01Icon = ({ icon: Icon }: A01IconProps) => {
-  return <Icon className={styles.A01Icon} />;
+const A01Icon = ({ className, icon: Icon }: A01IconProps) => {
+  return <Icon className={classNames(styles.A01Icon, className)} />;
 };
 
 export default A01Icon;
